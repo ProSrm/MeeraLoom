bb0db700fe3c57905fc47a4208582b7c53b17ebb
shubh_m <shubham.r.mate@softmass.co.in>
2025-02-12 00:08:55 +0530

    About us section , added services and separated footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
-import About from "./Components/About/About";
+import AboutUs from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Services from "./Components/Services/Services"
+import { Footer } from "./Components/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-    <p>&copy; 2025 IT Services. All rights reserved.</p>
-  </footer>
-);
 
 const App = () => (
   <Router>
     <Navbar />
     <Home />
     <Services />
-    <About />
+    <AboutUs />
     <Contact />
     {/* <Routes>
       <Route path="/" element={<Home />} />
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -5,6 +5,7 @@ import ServiceCard from '../Card';
 const Services = () => (
   <>
     <section className="py-10 px-6">
+      <h2 className="text-3xl font-semibold text-center">Our Services</h2>
       <div className="mt-6 grid md:grid-cols-3 gap-3">
         <ServiceCard
           title="Web Applications"
@@ -20,6 +21,22 @@ const Services = () => (
           title="Cloud Solutions"
           image=""
           description="Optimized cloud services for better scalability."
+        />
+
+        <ServiceCard
+          title="Software Development"
+          image=""
+          description="Tailored applications for businesses."
+        />
+        <ServiceCard
+          title="Machine Learning"
+          image=""
+          description="AI-powered solutions for automation."
+        />
+        <ServiceCard
+          title="System Integration"
+          image=""
+          description="Seamless integration to platforms."
         />
       </div>
     </section>
