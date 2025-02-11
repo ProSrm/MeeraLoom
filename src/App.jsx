790676d5f75f68d39933bcebfa1853c7c9b36c5c
Vishal <vishal.u.chinchane@softmass.co.in>
2025-02-11 15:28:57 +0530

    Change in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,9 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
-import Services from "./Components/Services/Services"
+import Services from "./Components/Services/Services";
+import Footer from "./Components/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-    <p>&copy; 2025 IT Services. All rights reserved.</p>
-  </footer>
-);
 
 const App = () => (
   <Router>
