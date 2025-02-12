8d888dac8af810ce29d2b5e3f56eb8b24ebb3981
Vishal <vishal.u.chinchane@softmass.co.in>
2025-02-12 11:43:45 +0530

    Merged Branch

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-const Footer = () => {
+import React from "react";
+
+export const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-10">
       <p>&copy; 2025 IT Services. All rights reserved.</p>
     </footer>
   );
 };
-
-export default Footer;
-=======
-import React from "react";
-
-export const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-            <p>&copy; 2025 IT Services. All rights reserved.</p>
-        </footer>
-    )
-};
-
-
-
->>>>>>> main
